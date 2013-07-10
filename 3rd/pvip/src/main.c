c050c0018f9abbeb089dc85961f1f1e3d155ca4b
tokuhirom <tokuhirom@gmail.com>
2013-07-11 08:30:00 +0900

    simplified code

--- 3rd/pvip/src/main.c
+++ 3rd/pvip/src/main.c
@@ -30,37 +30,28 @@ int main(int argc, char **argv) {
     command_parse(&cmd, argc, argv);
 
     if (state->eval) {
-            const char *src = state->eval;
-            PVIPNode *node = PVIP_parse_string(src, strlen(src), state->debug);
-            assert(node);
+        const char *src = state->eval;
+        PVIPNode *node = PVIP_parse_string(src, strlen(src), state->debug);
+        assert(node);
 
-            PVIPString *buf = PVIP_string_new();
-            PVIP_node_as_sexp(node, buf);
-            PVIP_string_say(buf);
-            PVIP_string_destroy(buf);
-            PVIP_node_destroy(node);
+        PVIP_node_dump_sexp(node);
+
+        PVIP_node_destroy(node);
     } else if (cmd.argc==1) {
-            FILE *fp = fopen(cmd.argv[0], "rb");
-            if (!fp) {
-                perror(cmd.argv[0]);
-                exit(0);
-            }
-            PVIPString *buf = PVIP_string_new();
-            PVIPNode *node = PVIP_parse_fp(fp, state->debug);
-            assert(node);
-            PVIP_node_as_sexp(node, buf);
-            PVIP_string_say(buf);
-            PVIP_string_destroy(buf);
-            PVIP_node_destroy(node);
-            fclose(fp);
+        FILE *fp = fopen(cmd.argv[0], "rb");
+        if (!fp) {
+            perror(cmd.argv[0]);
+            exit(0);
+        }
+        PVIPNode *node = PVIP_parse_fp(fp, state->debug);
+        PVIP_node_dump_sexp(node);
+        PVIP_node_destroy(node);
+        fclose(fp);
     } else if (cmd.argc==0) {
-            PVIPString *buf = PVIP_string_new();
-            PVIPNode *node = PVIP_parse_fp(stdin, state->debug);
-            assert(node);
-            PVIP_node_as_sexp(node, buf);
-            PVIP_string_say(buf);
-            PVIP_string_destroy(buf);
-            PVIP_node_destroy(node);
+        PVIPNode *node = PVIP_parse_fp(stdin, state->debug);
+        assert(node);
+        PVIP_node_dump_sexp(node);
+        PVIP_node_destroy(node);
     } else {
         command_help(&cmd);
     }
