f912a17bda1d651752bb23b0f1b667d4d7bdfaf5
tokuhirom <tokuhirom@gmail.com>
2013-07-13 17:13:23 +0900

    oops

---
+++ src/builtin.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void Kiji_bootstrap_Array(MVMCompUnit* cu, MVMThreadContext*tc);
+void Kiji_bootstrap_Str(MVMCompUnit* cu, MVMThreadContext*tc);
+void Kiji_bootstrap_Hash(MVMCompUnit* cu, MVMThreadContext*tc);
+void Kiji_bootstrap_File(MVMCompUnit* cu, MVMThreadContext*tc);
+void Kiji_bootstrap_Int(MVMCompUnit* cu, MVMThreadContext*tc);
+
+#ifdef __cplusplus
+}
+#endif
